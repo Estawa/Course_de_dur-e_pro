2c183b482d789ea5e398b0af285956f01798435e
Estawa <cguilhem77310@gmail.com>
2026-09-04 17:47:49 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
       manifest: {
         name: "Course de Durée Pro by C. Guilhem",
         short_name: "Course Durée Pro",
